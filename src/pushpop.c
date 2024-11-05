ffe1a64628a37d685680aadf6dbe9920943d4f27
Marc Sune <marcdevel@gmail.com>
2024-11-05 20:33:00 +0100

    Tc

--- src/pushpop.c
+++ src/pushpop.c
@@ -16,7 +16,7 @@
 #define HDR_SIZE 8 //20
 #define CHECK_SKB_PTR_VAL( SKB, PTR, VAL ) \
 	if (  (void*)(PTR) > ((void*)(unsigned long long) SKB -> data_end )) {	\
-		bpf_printk("[%p] ptr %p on pkt with length: %d out of bounds!\n",\
+		bpf_printk("[0x%llx] ptr %p on pkt with length: %d out of bounds!\n",\
 							SKB, PTR, SKB ->len);	\
 		return VAL;							\
 	} do{}while(0)
@@ -37,129 +37,13 @@ __be16 csum_fold(__s64 csum)
 	return (__be16)~csum;
 }
 
-static __always_inline __attribute__((__unused__))
-int push_udp(struct __sk_buff *skb, struct ethhdr *eth, struct iphdr* ip,
-						struct tcphdr *tcp)
-{
-	int rc;
-	struct iphdr old_ip;
-	struct udphdr udp, *udp_ptr;
-	struct pseudo_header old, new;
-	__u32 l3_off  = (__u8*)ip - (__u8*)eth;
-	__u16 tot_len;
-
-	//Easier to diff
-	old_ip = *ip;
-
-	//L3 changes
-	ip->protocol = 17;
-	tot_len = bpf_ntohs(ip->tot_len) + HDR_SIZE;
-	ip->tot_len = bpf_htons(tot_len);
-
-	//Adjust L3 csum and make room for UDP (so that we only need to reeval
-	//ptrs one time).
-	__s64 diff = bpf_csum_diff((__be32*)&old_ip, 4, (__be32*)ip, 4, 0);
-	diff = bpf_csum_diff((__be32*)&old_ip.ttl, 4, (__be32*)&ip->ttl, 4, diff);
-
-	//Adjust IP checksum and make room for the new L4 hdr
-	rc = bpf_l3_csum_replace(skb, l3_off + offsetof(struct iphdr, check),
-							0,
-							diff, 0);
-	if (rc < 0) {
-		bpf_printk("[%p] Unable to l3_csum_replace: %d", skb, rc);
-		return rc;
-	}
-	rc = bpf_skb_adjust_room(skb, HDR_SIZE, BPF_ADJ_ROOM_NET, 0);
-	if (rc < 0) {
-		bpf_printk("[%p] Unable to bpf_skb_adjust_room: %d", skb, rc);
-		return rc;
-	}
-
-	//Reval ptrs
-	eth = (void *)(unsigned long long)skb->data;
-	CHECK_SKB_PTR(skb, eth+1);
-	ip = (struct iphdr*) (eth+1);
-	CHECK_SKB_PTR(skb, ip+1);
-	udp_ptr = (struct udphdr*) (ip+1);
-	CHECK_SKB_PTR(skb, udp_ptr+1);
-	tcp = (struct tcphdr*) (udp_ptr+1);
-	CHECK_SKB_PTR(skb, tcp+1);
-
-	//Set UDP encap info
-	udp.source = tcp->source;
-	udp.dest = bpf_htons(TUN_PORT);
-	udp.check = 0;
-	udp.len = bpf_htons(tot_len - (ip->ihl * 4));
-
-	//Take TCP csum as basis (payload)
-	diff = csum_fold(tcp->check);
-
-	//pseudohdr csum_diff
-	old.res = 0x0;
-	old.proto = 6;
-	old.len = bpf_htons(tot_len - HDR_SIZE - (ip->ihl * 4));
-
-	new = old;
-	new.proto = 17;
-	new.len = bpf_htons(tot_len - (ip->ihl * 4));
-	diff = bpf_csum_diff((__be32*)&old, sizeof(old), (__be32*)&new,
-							sizeof(new), diff);
-
-	//Adjust old L4 csum (0ed TCP csum, now payload)
-	__be16 tcp_csum[2] = {tcp->check, 0};
-	diff = bpf_csum_diff(0, 0, (__be32*)&tcp_csum, 4, diff);
-
-	//Add new HDR
-	diff = bpf_csum_diff(0, 0, (__be32*)&udp, sizeof(udp), diff);
-	udp.check = csum_fold(diff);
-	*udp_ptr = udp;
-
-	return TC_ACT_UNSPEC;
-}
-
-static __always_inline __attribute__((__unused__))
-int pop_udp(struct __sk_buff *skb, struct ethhdr *eth, struct iphdr* ip,
-						struct udphdr *udp)
-{
-	int rc;
-	struct iphdr old_ip;
-	__u32 l3_off  = (__u8*)ip - (__u8*)eth;
-	__u16 tot_len;
-
-	//Easier to diff
-	old_ip = *ip;
-
-	//UDP decap
-	ip->protocol = 6;
-	tot_len = bpf_ntohs(ip->tot_len) - HDR_SIZE;
-	ip->tot_len = bpf_htons(tot_len);
-
-	//total_len
-	__s64 diff = bpf_csum_diff((__be32*)&old_ip, 4, (__be32*)ip, 4, 0);
-	diff = bpf_csum_diff((__be32*)&old_ip.ttl, 4, (__be32*)&ip->ttl, 4, diff);
-
-	//Adjust IP checksum and make room for the new L4 hdr
-	rc = bpf_l3_csum_replace(skb, l3_off + offsetof(struct iphdr, check),
-							0,
-							diff, 0);
-	if (rc < 0) {
-		bpf_printk("[%p] Unable to l3_csum_replace: %d", skb, rc);
-		return rc;
-	}
-	rc = bpf_skb_adjust_room(skb, -HDR_SIZE, BPF_ADJ_ROOM_NET, 0);
-	if (rc < 0) {
-		bpf_printk("[%p] Unable to bpf_skb_adjust_room: %d", skb, rc);
-		return rc;
-	}
-
-	return TC_ACT_UNSPEC;
-}
-
 SEC("classifier")
 int tcponudp(struct __sk_buff *skb)
 {
+	int rc;
 	struct ethhdr *eth;
 	struct iphdr* ip;
+	struct tcphdr *tcp;
 
 	eth = (void *)(unsigned long long)skb->data;
 	CHECK_SKB_PTR(skb, eth+1);
@@ -170,33 +54,27 @@ int tcponudp(struct __sk_buff *skb)
 	ip = (struct iphdr*) (eth+1);
 	CHECK_SKB_PTR(skb, ip+1);
 
-#ifdef PUSH
-	struct tcphdr *tcp;
-
 	if (ip->protocol != IPPROTO_TCP)
 		return TC_ACT_UNSPEC;
 
-	tcp = (struct tcphdr*) (ip+1);
+	tcp = (struct tcphdr *) ((__u8*)ip + (ip->ihl * 4));
 	CHECK_SKB_PTR(skb, tcp+1);
 
 	if (tcp->dest != bpf_htons(PORT))
 		return TC_ACT_UNSPEC;
 
-	return push_udp(skb, eth, ip, tcp);
-#else //PUSH
-	struct udphdr *udp;
-
-	if (ip->protocol != IPPROTO_UDP)
-		return TC_ACT_UNSPEC;
-
-	udp = (struct udphdr*) (ip+1);
-	CHECK_SKB_PTR(skb, udp+1);
-
-	if (udp->dest != bpf_htons(TUN_PORT))
-		return TC_ACT_UNSPEC;
-
-	return pop_udp(skb, eth, ip, udp);
-#endif //PUSH
+#if 10
+	//Disable offlods (workaround)
+	rc = bpf_skb_change_tail(skb, skb->len, 0);
+	(void)rc;
+	if (rc < 0) {
+		bpf_printk("[%p] Unable to bpf_skb_change_tail_1: %d", skb, rc);
+		return rc;
+	}
+#else
+	(void)rc;
+#endif
+	return TC_ACT_UNSPEC;
 }
 
 char ____license[] __attribute__((section("license"), used)) = "Dual BSD/GPL";
